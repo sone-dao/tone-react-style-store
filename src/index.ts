fed6bf545a1e2c44a0fc826916ea8b0ae80c0257
Lon Beshiri <lonbeshiri@gmail.com>
2023-12-20 15:59:41 -0500

    feat: initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
-import ClientComponent from './ClientComponent'
-import ServerComponent from './ServerComponent'
+import { StoreApi, UseBoundStore, create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
-export { ClientComponent, ServerComponent }
+export type ToneStyles = {
+  global: [string, string]
+  uploader: [string, string]
+  user: [string, string]
+}
+
+export type UseStyleStore = UseBoundStore<StoreApi<ToneStyles>>
+
+export default create<ToneStyles>()(
+  persist(
+    (set) => ({
+      global: ['', ''],
+      uploader: ['', ''],
+      user: ['', ''],
+    }),
+    { name: 'tone.styles' }
+  )
+)
